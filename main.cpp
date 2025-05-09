3efcde0117c3fba0fe3eb912536ca2e855beb3fd
Pilastro22 <chiara.cafiso@edu-its.it>
2025-05-09 16:05:56 +0000

    salvataggio

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main ()
-{
-  cout<<"Hello world!!!"<<endl;
-  return 0;
+
+int main(){
+string a;
+int b;
+cout<<"Ciao, come ti chiami?"<<endl;
+cin>>a;
+cout<<"Quanti anni hai?"<<endl;
+cin>>b;
+cout<<"Ciao "<<a<<" , hai "<<b<<" anni."<<endl;
+cout<<"-----------------------------------------"<<endl;
+cout<<left<<setw(40)<<"| DETTAGLI   PERSONALI "<<right<<"|"<<endl;
+cout<<"-----------------------------------------"<<endl;
+cout<<left<<setw(33)<<"| Nome: "<<right<<a<<"|"<<endl;
+
+
+
+
 }
